4a50a54a6e4aa9d26f481060f152f3f5737e3b45
Artur Coelho Paz de Sousa <1241302313@acd.grupotiradentes.com>
2024-06-07 20:00:09 -0300

    Adicao da renderizacao de produtos via JavaScript

---
+++ script/json-fetch.js
@@ -0,0 +1,18 @@
+const fetchData = async () => {
+    return fetch('../script/produtos.json')
+      .then(async response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const fetchedData = await response.json();
+        return fetchedData
+      })
+       .then(data => {
+        return { data };
+      })
+      .catch(error => {
+        return { error };
+      });
+  };
+
+export { fetchData } 
