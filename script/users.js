d5403ffe958a0a9401cd3219553db3c9e11a74cd
Alice Pereira <alicecavalcanti24@gmail.com>
2024-06-12 01:12:15 -0300

    finalizando fluxo de pagamento

---
+++ script/users.js
@@ -0,0 +1,14 @@
+class User {
+    
+    constructor(name, email, password) {
+        name = name
+        email = email
+        password = password
+        idade = ''
+        genero = ''
+        telefone = ''
+        CEP = ''
+    }
+}
+
+export { User }
